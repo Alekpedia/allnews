255518a65e7805672678d0dc9526022ca5ab7196
Alek Sander <50556243+Alekpedia@users.noreply.github.com>
2020-12-17 11:42:47 +0700

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/example/aleks/allnews/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.aleks.allnews;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.aleks.allnews";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String NEWS_API_TOKEN = "12850cd010b54441aaeff6749dc99cd0";
+}
